b313d2dfafd9f07bba4ca22dc9df24e33573d08d
Luke Wang <lukewang63@gmail.com>
2023-09-17 04:52:16 -0400

    Added endpoint to check similarity between two words

--- flaskapi.py
+++ flaskapi.py
@@ -1,7 +1,12 @@
 from flask import Flask, request
-import os, cockroach
+import os, cockroach, keys, cohere, numpy as np
 
 app = Flask(__name__)
+co = cohere.Client(keys.cohere_key)
+
+
+def calculate_similarity(a, b):
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 
 
 @app.route("/api")
@@ -14,6 +19,17 @@ def item_from_id():
     ids = request.args["id"]
     item = cockroach.get_item_by_id(ids)
     return {"name": item[1], "url": item[2]}
+
+
+@app.route("/api/word-similarity")
+def similarity():
+    ans = request.args["ans"]
+    guess = request.args["guess"]
+
+    # get the embeddings
+    (ans, guess) = co.embed(texts=[ans, guess]).embeddings
+
+    return {"similarity": calculate_similarity(ans, guess)}
 
 
 if __name__ == "__main__":
